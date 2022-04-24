b5802ef0ecf30f7e5012e5e3c8a960b00dfa303b
fahmida43 <99779584+fahmida43@users.noreply.github.com>
2022-04-24 08:19:24 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var num = 100;
+if(num >= 80 && num <= 100) {
+    document.write( 'A+');
+} if else(num >= 70 num <= 79) {
+    document.write ('A');
+} if else(num >= 60 num <= 69) {
+    document.write ('A-');
+} if else(num >= 50 num <= 59) {
+    document.write ('B');
+} if else(num >= 40 num <= 49) {
+    document.write ('C');
+} if else(num >= 33 num <= 39) {
+    document.write ('D');
+} else {
+    document.write (' Failed')
+}
